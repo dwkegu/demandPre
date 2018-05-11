8f2bc4a27c42187ce28a8289100a64a9d2632a94
weiruan peng <dwkegu@gmail.com>
2018-05-11 20:58:44 +0800

    add info

--- src/model/STC_LSTMCell.py
+++ src/model/STC_LSTMCell.py
@@ -27,7 +27,7 @@ class STC_LSTMCell(RNNCell):
         if params is None:
             # t_size t_stride channel_size
             # params = [[[12, 12, 12], [24, 24, 24]], [[1, 5, 5, 36, output_shape[-1]], [1, 1, 1, 1, 1]]]
-            params = [[[24, 24, 24], [48, 48, 48]], [[1, 5, 5, 72, output_shape[-1]], [1, 1, 1, 1, 1]]]
+            params = [[[24, 24, 12], [48, 48, 24]], [[1, 5, 5, 36, output_shape[-1]], [1, 1, 1, 1, 1]]]
         self._params = params
         self.conv_variables = {}
 
