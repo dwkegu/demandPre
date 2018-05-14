1b208c00a4c37e4cda1980c5246661df652a01f8
weiruan peng <dwkegu@gmail.com>
2018-05-15 01:37:09 +0800

    modified lrn

--- src/model/model.py
+++ src/model/model.py
@@ -47,69 +47,70 @@ class Model:
             sess.run([initial])
             min_valid_score = 100
             best_model_index = 92
-            # for i in range(epoches):
-            #     save = True
-            #     start_time = time.time()
-            #     train_data = dataset.get_train_batch()
-            #     total_loss = 0
-            #     print("train epoch %d " % i, end="")
-            #     for t_x, t_y in train_data:
-            #         [y, loss, _] = sess.run([self._y, self._loss, self._train_op],
-            #                              feed_dict={self._inputs: t_x, self._outputs: t_y})
-            #         total_loss += loss
-            #         print("=", end="")
-            #         if save:
-            #             np.save(os.path.join(config.log_path, "output", "output-%d.npy" % i), y)
-            #             np.save(os.path.join(config.log_path, "output", "label-%d.npy" % i), t_y)
-            #             save = False
-            #     rmse = np.sqrt(total_loss / dataset.get_train_epoch_size())
-            #     if self._normalize:
-            #         rmse = dataset._normalor.restoreLoss(rmse)
-            #     print("")
-            #     print("training epoch %d, loss is %f, rmse is %f" % (
-            #     i, total_loss, rmse))
-            #     if dataset.hasValidData:
-            #         valid_data = dataset.get_valid_batch()
-            #         total_loss = 0
-            #         print("valid", end="")
-            #         for t_x, t_y in valid_data:
-            #             [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
-            #             total_loss += loss
-            #             print("=", end="")
-            #             # print(loss)
-            #         now = time.time()
-            #         valid_rmse = np.sqrt(total_loss / dataset.get_valid_epoch_size())
-            #         if self._normalize:
-            #             valid_rmse = dataset._normalor.restoreLoss(valid_rmse)
-            #         print("\ntime is %ds valid rmse is %f " % (now - start_time, valid_rmse))
-            #         if valid_rmse < min_valid_score:
-            #             saver.save(sess, os.path.join(self._model_path, "saved_model"), global_step=i + 1)
-            #             print("model-%s saved." % (i + 1))
-            #             best_model_index = i + 1
-            #             min_valid_score = valid_rmse
-            #     else:
-            #         test_data = dataset.get_test_batch()
-            #         total_loss = 0
-            #         for t_x, t_y in test_data:
-            #             [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
-            #             total_loss += loss
-            #         test_rmse = np.sqrt(total_loss / dataset.get_test_epoch_size())
-            #         now = time.time()
-            #         if self._normalize:
-            #             test_rmse = dataset._normalor.restoreLoss(test_rmse)
-            #         print("time si %d test rmse is %f " % ((now - start_time), test_rmse))
-            #     if i % 5 == 0:
-            #         if dataset.hasValidData:
-            #             # saver.restore(sess, self._model_path + "-" + str(best_model_index))
-            #             test_data = dataset.get_test_batch()
-            #             total_loss = 0
-            #             for t_x, t_y in test_data:
-            #                 [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
-            #                 total_loss += loss
-            #             test_rmse = np.sqrt(total_loss / dataset.get_test_epoch_size())
-            #             if self._normalize:
-            #                 test_rmse = dataset._normalor.restoreLoss(test_rmse)
-            #             print("test rmse is %f " % test_rmse)
+            for i in range(epoches):
+                save = True
+                start_time = time.time()
+                train_data = dataset.get_train_batch()
+                total_loss = 0
+                print("train epoch %d " % i, end="")
+                for t_x, t_y in train_data:
+                    [y, loss, _] = sess.run([self._y, self._loss, self._train_op],
+                                         feed_dict={self._inputs: t_x, self._outputs: t_y})
+                    total_loss += loss
+                    print("=", end="")
+                    if save:
+                        np.save(os.path.join(config.log_path, "output", "output-%d.npy" % i), y)
+                        np.save(os.path.join(config.log_path, "output", "label-%d.npy" % i), t_y)
+                        save = False
+                rmse = np.sqrt(total_loss / dataset.get_train_epoch_size())
+                if self._normalize:
+                    rmse = dataset._normalor.restoreLoss(rmse)
+                print("")
+                print("training epoch %d, loss is %f, rmse is %f" % (
+                i, total_loss, rmse))
+                if dataset.hasValidData:
+                    valid_data = dataset.get_valid_batch()
+                    total_loss = 0
+                    print("valid", end="")
+                    for t_x, t_y in valid_data:
+                        [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
+                        total_loss += loss
+                        print("=", end="")
+                        # print(loss)
+                    now = time.time()
+                    valid_rmse = np.sqrt(total_loss / dataset.get_valid_epoch_size())
+                    if self._normalize:
+                        valid_rmse = dataset._normalor.restoreLoss(valid_rmse)
+                    print("\ntime is %ds valid rmse is %f " % (now - start_time, valid_rmse))
+                    if valid_rmse < min_valid_score:
+                        path = saver.save(sess, os.path.join(self._model_path, "saved_model"), global_step=i + 1)
+                        print(path)
+                        print("model-%s saved." % (i + 1))
+                        best_model_index = i + 1
+                        min_valid_score = valid_rmse
+                else:
+                    test_data = dataset.get_test_batch()
+                    total_loss = 0
+                    for t_x, t_y in test_data:
+                        [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
+                        total_loss += loss
+                    test_rmse = np.sqrt(total_loss / dataset.get_test_epoch_size())
+                    now = time.time()
+                    if self._normalize:
+                        test_rmse = dataset._normalor.restoreLoss(test_rmse)
+                    print("time si %d test rmse is %f " % ((now - start_time), test_rmse))
+                if i % 5 == 0:
+                    if dataset.hasValidData:
+                        # saver.restore(sess, self._model_path + "-" + str(best_model_index))
+                        test_data = dataset.get_test_batch()
+                        total_loss = 0
+                        for t_x, t_y in test_data:
+                            [loss] = sess.run([self._loss], feed_dict={self._inputs: t_x, self._outputs: t_y})
+                            total_loss += loss
+                        test_rmse = np.sqrt(total_loss / dataset.get_test_epoch_size())
+                        if self._normalize:
+                            test_rmse = dataset._normalor.restoreLoss(test_rmse)
+                        print("test rmse is %f " % test_rmse)
             if dataset.hasValidData:
                 saver.restore(sess, os.path.join(self._model_path, "saved_model-") + str(best_model_index))
                 test_data = dataset.get_test_batch()
