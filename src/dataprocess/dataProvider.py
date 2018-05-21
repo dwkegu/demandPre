c1d33f71a67dcbbf920402b6adf4e2c3aee8cc03
weiruan peng <dwkegu@gmail.com>
2018-05-21 13:59:30 +0800

    add support for cube cnn

--- src/dataprocess/dataProvider.py
+++ src/dataprocess/dataProvider.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from demandPre.src.dataprocess import nyprocess
+from demandPre.src.dataprocess import nyprocess, bjprocessor
+from demandPre.src.utils.utils import Normalor
 
 
 class DataProvider:
@@ -24,30 +25,105 @@ class DataProvider:
         pass
 
 
-class DidiDataProvider(DataProvider):
+class CNNDataProvider(DataProvider):
 
-    def __init__(self, filenames, batch_size, input_size, output_size, train_proprotion=0.8):
-        super(DidiDataProvider, self).__init__(filenames, batch_size, input_size, output_size)
-        if filenames.endswith('.npy'):
+    def __init__(self, filenames, batch_size, input_size, output_size, splits=None, output_reduce_channel=1,
+                 train_proprotion=None, offset=True, normalize=False):
+        super(CNNDataProvider, self).__init__(filenames, batch_size, input_size, output_size)
+        if train_proprotion is None:
+            train_proprotion = [0.8, 0.1, 0.1]
+        self._output_reduce_channel = output_reduce_channel
+        if isinstance(filenames, str) and filenames.endswith('.npy'):
             self.data = np.load(filenames)
             self.data = np.transpose(self.data, [2, 1, 0])
             self.data = np.expand_dims(self.data, 3)
-        elif filenames.endswith('.mat'):
+        elif isinstance(filenames, str) and filenames.endswith('mat'):
             self.data = nyprocess.load_data(filenames)
             self.data = np.expand_dims(self.data, 3)
-        self.time_length = self.data.shape[0]
-        self.train_length = int(self.time_length * train_proprotion)
-        self.test_length = self.time_length - self.train_length
-        self.train_data = self.data[0:self.train_length, :, :, :]
-        self.test_data = self.data[self.train_length:self.time_length, :, :, :]
-        s = np.sum(np.power(self.train_data, 2))
-        count = np.prod(self.train_data.shape)
-        s = np.sqrt(s/count)
-        print("average of train is %f" % s)
-        s = np.sum(np.power(self.test_data, 2))
-        count = np.prod(self.test_data.shape)
-        s = np.sqrt(s / count)
-        print("average of test is %f" % s)
+        elif isinstance(filenames, str) and filenames.endswith(".h5"):
+            self.data = nyprocess.load_nyb_data(filenames)
+        elif isinstance(filenames, (list, tuple)):
+            self.data = bjprocessor.load_bj_data(filenames)
+        if normalize:
+            self._normalor = Normalor(self.data)
+            self.data = self._normalor.fit()
+        self.data_offset = input_size + self._output_size - 1
+        self.hasValidData = True
+        if splits is None:
+            if len(train_proprotion) == 3:
+                self.hasValidData = True
+            else:
+                self.hasValidData = False
+            self.time_length = self.data.shape[0]
+            self.train_length = int(self.time_length * train_proprotion[0])
+            self.train_data = self.data[0:self.train_length, :, :, :]
+            if offset:
+                self.valid_length = int(self.time_length * train_proprotion[1])
+                self.test_length = self.time_length - self.train_length - self.valid_length
+                self.valid_data = self.data[self.train_length - self.data_offset:self.train_length + self.valid_length,
+                                  :, :, :]
+                self.test_data = self.data[self.train_length + self.valid_length - self.data_offset:self.time_length, :,
+                                 :, :]
+            else:
+                self.valid_length = int(self.time_length * train_proprotion[1]) - self.data_offset
+                self.test_length = self.time_length - self.train_length - self.valid_length - self.data_offset
+                self.valid_data = self.data[self.train_length:self.train_length + self.valid_length, :, :, :]
+                self.test_data = self.data[self.train_length + self.valid_length:self.time_length, :, :, :]
+        else:
+            if len(splits) == 3:
+                self.hasValidData = True
+                self.train_length = splits[0]
+                self.valid_length = splits[1] if offset else splits[1] - self.data_offset
+                self.test_length = splits[2] if offset else splits[2] - self.data_offset
+                self.train_data = self.data[0:splits[0], :, :, :]
+                if offset:
+                    self.valid_data = self.data[splits[0] - self.data_offset:splits[0] + splits[1], :, :, :]
+                    self.test_data = self.data[
+                                     splits[0] + splits[1] - self.data_offset:splits[0] + splits[1] + splits[2],
+                                     :, :, :]
+                else:
+                    self.valid_data = self.data[splits[0]:splits[0] + splits[1], :, :, :]
+                    self.test_data = self.data[
+                                     splits[0] + splits[1]:splits[0] + splits[1] + splits[2],
+                                     :, :, :]
+            elif len(splits) == 2:
+                self.hasValidData = True
+                self.valid_length = splits[0]
+                self.test_length = splits[1]
+                _data = self.data[-1]
+                self.valid_data = _data[-(splits[0] + splits[1] + self.data_offset):, :, :, :, :]
+                self.test_data = _data[- (self.test_length + self.data_offset):, :, :, :, :]
+                for i, item in enumerate(self.data):
+                    if i == len(self.data):
+                        self.train_data.append(item[:-(splits[0] + splits[1])])
+                    else:
+                        self.train_data.append(item)
+                self.data = np.concatenate(self.data)
+            elif len(splits) == 1 and isinstance(self.data, (list, tuple)):
+                self.hasValidData = False
+                self.test_data = self.data[-1]
+                self.test_length = splits[0]
+                self.test_data = self.test_data[-(splits[0] + self.data_offset):]
+                self.train_data = []
+                for i, item in enumerate(self.data):
+                    if i == len(self.data):
+                        self.train_data.append(item[:splits[0]])
+                    else:
+                        self.train_data.append(item)
+                self.data = np.concatenate(self.data)
+        if isinstance(self.train_data, np.ndarray):
+            s = np.sum(np.power(self.train_data, 2), dtype=np.int64)
+            count = np.prod(self.train_data.shape)
+            s = np.sqrt(s / count)
+            print("average of train is %f" % s)
+            s = np.sum(np.power(self.valid_data, 2))
+            count = np.prod(self.valid_data.shape)
+            s = np.sqrt(s / count)
+            print("average of valid is %f" % s)
+            s = np.sum(np.power(self.test_data, 2))
+            count = np.prod(self.test_data.shape)
+            s = np.sqrt(s / count)
+            print("average of test is %f" % s)
 
     def get_train_batch(self):
         position = 0
@@ -77,7 +153,38 @@ class DidiDataProvider(DataProvider):
                 yield (x, y)
 
     def get_train_epoch_size(self):
-        return (self.train_length - self._input_size - self._output_size + 1) * self.data.shape[1] * self.data.shape[2] * self.data.shape[3]
+        return (self.train_length - self._input_size - self._output_size + 1) * self.data.shape[1] * self.data.shape[2] * (self.data.shape[3] - self._output_reduce_channel)
+
+    def get_valid_batch(self):
+        position = 0
+        while position + self._input_size + self._output_size - 1 < self.valid_length:
+            if position + self._batch_size + self._input_size + self._output_size - 1 >= self.valid_length:
+                x = []
+                y = []
+                for i in range(position, self.valid_length - self._input_size - self._output_size + 1):
+                    sample_x = self.valid_data[i:i + self._input_size, :, :, :]
+                    sample_y = self.valid_data[
+                               i + self._input_size:i + self._input_size + self._output_size,
+                               :, :, :]
+                    x.append(sample_x)
+                    y.append(sample_y)
+                position = self.valid_length - self._input_size - self._output_size + 1
+                yield (x, y)
+            else:
+                x = []
+                y = []
+                for i in range(position, self._batch_size + position):
+                    sample_x = self.valid_data[i:i + self._input_size, :, :, :]
+                    sample_y = self.valid_data[
+                               i + self._input_size:i + self._input_size + self._output_size,
+                               :, :, :]
+                    x.append(sample_x)
+                    y.append(sample_y)
+                position += self._batch_size
+                yield (x, y)
+
+    def get_valid_epoch_size(self):
+        return (self.valid_length - self._input_size - self._output_size + 1) * self.data.shape[1] * self.data.shape[2] * self.data.shape[3]
 
     def get_test_batch(self):
         position = 0
--- src/main.py
+++ src/main.py
@@ -11,25 +11,35 @@ sys.path.append(project_path)
 # from dataprocess import didiprocess
 from demandPre.src import config
 from demandPre.src.model.CubeCNN import CubeCNN
-from demandPre.src.dataprocess.dataProvider import DidiDataProvider
+from demandPre.src.dataprocess.dataProvider import CNNDataProvider
 from demandPre.src.dataprocess.STC_DataProvider import STC_Provider
 from demandPre.src.model.STC_LSTM import STC_Lstm
 
-os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >' + config.log_path + '/tmp')
-memory_gpu=[int(x.split()[2]) for x in open(config.log_path + '/tmp', 'r').readlines()]
-print(memory_gpu)
-os.environ["CUDA_VISIBLE_DEVICES"] = str(np.argmax(memory_gpu))
+# os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >' + config.log_path + '/tmp')
+# memory_gpu=[int(x.split()[2]) for x in open(config.log_path + '/tmp', 'r').readlines()]
+# print(memory_gpu)
+# os.environ["CUDA_VISIBLE_DEVICES"] = str(np.argmax(memory_gpu))
 
 if __name__ == '__main__':
     # [batch, T, d, h, w c]
-    nyt_config = {'input_shape': [None, 4, 24*7, 64, 64, 1], 'output_shape': [None, 1, 64, 64, 1],
+    nyt_config_lstm = {'input_shape': [None, 4, 24*7, 64, 64, 1], 'output_shape': [None, 1, 64, 64, 1],
                   'model_name': "nyc-taxi-STC-LSTM-4w-168h-step", 't_length': 4, 'T': 24 * 7, 'splits':[14640, 1440, 1440], 'offset':False}
-    nyb_config = {'input_shape': [None, 28, 24, 16, 8, 1], 'output_shape': [None, 1, 16, 8, 1],
+    nyb_config_lstm = {'input_shape': [None, 28, 24, 16, 8, 1], 'output_shape': [None, 1, 16, 8, 1],
                   'model_name': "nyc-bike-STC-LSTM-7d", 't_length': 28, 'T': 24, 'splits':[3737, 415, 240], 'offset':True}
-    cd_didi_config = {'input_shape': [None, 7, 96, 32, 32, 1], 'output_shape': [None, 1, 32, 32, 1],
+    cd_didi_config_lstm = {'input_shape': [None, 7, 96, 32, 32, 1], 'output_shape': [None, 1, 32, 32, 1],
                       'model_name': "didi-taxi-STC-LSTM", 't_length': 7, 'T': 96, 'splits':[2304, 288, 288], 'offset':True}
-    m_config = nyb_config
-    model = STC_Lstm(m_config['input_shape'], m_config['output_shape'], learning_rate=0.0002,
+    nyt_config_cnn = {"input_shape":[None, 24 * 7, 64, 64, 2], 'output_shape': [None, 1, 64, 64, 1],
+                      'model_name':"ntc-taxi-cnn-7d", "splits":[14640, 1440, 1440], 'offset':False}
+    nyb_config_cnn = {'input_shape': [None, 24 * 7, 16, 8, 1], 'output_shape': [None, 1, 16, 8, 1],
+                       'model_name': "nyc-bike-STC-CNN-7d", 'splits': [3737, 415, 240], 'offset': True}
+    cd_didi_config_cnn = {'input_shape': [None, 96 * 7, 32, 32, 1], 'output_shape': [None, 1, 32, 32, 1],
+                           'model_name': "didi-taxi-cnn-7d", 'splits': [2304, 288, 288], 'offset': True}
+    m_config = nyb_config_cnn
+    model_select = "CubeCNN" # STC-LSTM
+    if model_select == "CubeCNN":
+        model = CubeCNN(m_config["input_shape"], m_config["output_shape"], name=m_config["model_name"])
+    else:
+        model = STC_Lstm(m_config['input_shape'], m_config['output_shape'], learning_rate=0.0002,
                      name=m_config['model_name'], normalize=False)
     # filenames = os.listdir(config.dataset_path)
     # files = ["BJ13_M32x32_T30_InOut.h5", "BJ14_M32x32_T30_InOut.h5", "BJ15_M32x32_T30_InOut.h5", "BJ16_M32x32_T30_InOut.h5"]
@@ -37,7 +47,11 @@ if __name__ == '__main__':
     # print(allFiles)
     # dataset = STC_Provider(filenames=allFiles, t_length=7, batch_size=48, input_size=48, output_size=1, splits=[10248, 1128, 1344])
     # dataset = STC_Provider(config.dataset_path + "/NYC14_M16x8_T60_NewEnd.h5", 7, 16, 24, 1, [3737, 415, 240])
-    dataset = STC_Provider(config.dataset_path + "/NYC14_M16x8_T60_NewEnd.h5", m_config['t_length'], 24,
+    if model_select == "CubeCNN":
+        dataset = CNNDataProvider(config.dataset_path + "/NYC14_M16x8_T60_NewEnd.h5", 16, m_config["input_shape"][1],
+                                  m_config["output_shape"][-1], splits=m_config["splits"])
+    else:
+        dataset = STC_Provider(config.dataset_path + "/NYC14_M16x8_T60_NewEnd.h5", m_config['t_length'], 24,
                            m_config['T'], 1, m_config['splits'], output_reduce_channel=0, offset=m_config['offset'],
                            normalize=False)
     model.fit(dataset, 100)
