bcef882c296c02524f38a2741c63150cd5033cca
weiruan peng <dwkegu@gmail.com>
2018-05-22 15:12:37 +0800

    add support for cube cnn

--- src/model/CubeCNN.py
+++ src/model/CubeCNN.py
@@ -63,7 +63,7 @@ class CubeCNN(Model):
         with tf.name_scope("layer1"):
             net = self.block_A(self._inputs, [[5, 8], [9, 8], [12, 8], [24, 8]], activation=tf.nn.relu, name="layer1_blockA")
         with tf.name_scope("layer2"):
-            net = self.block_B(net, [[[5, 5, 32, 16], [1, 1, 1, 1], 16], [[3, 3, 8, 8], [1, 1, 1, 1], 8], [[3, 3, 8, 1], [1, 1, 1, 1], 1]],
+            net = self.block_B(net, [[[5, 5, 32, 16], [1, 1, 1, 1], 16], [[3, 3, 16, 8], [1, 1, 1, 1], 8], [[3, 3, 8, 1], [1, 1, 1, 1], 1]],
                                activation=tf.nn.relu, name="layer2_blockB")
         net = tf.reshape(net, [-1, 1, net.shape[1], net.shape[2], 1])
         loss = 2 * tf.nn.l2_loss(net-self._outputs)
